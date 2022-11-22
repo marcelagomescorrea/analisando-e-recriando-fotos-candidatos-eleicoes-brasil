4e4cf0a0fa36327c25332ea94badcd20871dc392
Marcela Correa <marcelagomescorrea@gmail.com>
2022-11-22 12:39:54 -0300

    incluindo SG_UF nas colunas do dataframe criado a partir do csv

--- backend/data_source/params.py
+++ backend/data_source/params.py
@@ -1,3 +1,3 @@
-COLUMN_NAMES = ['NM_CANDIDATO', 'DS_CARGO', 'SQ_CANDIDATO', 'DS_SIT_TOT_TURNO', 'CD_SIT_TOT_TURNO', 'ANO_ELEICAO', 'SG_UE']
+COLUMN_NAMES = ['NM_CANDIDATO', 'DS_CARGO', 'SQ_CANDIDATO', 'DS_SIT_TOT_TURNO', 'CD_SIT_TOT_TURNO', 'ANO_ELEICAO', 'SG_UF']
 COLUMN_NAMES_FULL = COLUMN_NAMES + ['filename']
 states = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO', 'DF']
